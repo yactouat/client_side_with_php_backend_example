eae27c13e8903037e24d985d4e4971808de46e07
yactouat <yactouat@hotmail.com>
2022-11-06 16:47:42 +0100

    output items images

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `item` (
   `id` int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  `title` varchar(255) NOT NULL
+  `title` varchar(255) NOT NULL,
+  `image_url` varchar(255) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `item` (`title`) VALUES
-('foo'),
-('bar'),
-('baz');
+INSERT INTO `item` (`title`, `image_url`) VALUES
+('foo', 'https://picsum.photos/200'),
+('bar', 'https://picsum.photos/200'),
+('baz', 'https://picsum.photos/200');
